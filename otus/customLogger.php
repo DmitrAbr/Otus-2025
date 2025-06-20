e3a65244653125749117b42f83cd50f975d221e1
дмитрий <dmitrij.abramov.2003_7@mail.ru>
2025-06-20 09:11:20 +0700

    chore(create): Created new php file - customLogger.php

---
+++ otus/customLogger.php
@@ -0,0 +1,6 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->setTitle("Otus/customLogger.php");
+
+$c = 1 / 0;
+?>Вы открыли страницу otus/customLogger.php и записали в лог свое посещение<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
